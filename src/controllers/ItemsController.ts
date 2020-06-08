f963a2143dd53f421d91aa9c94f926e53a2f4a50
Johny Lopes <jlopes@metatron.com.br>
2020-06-07 21:04:01 -0300

    adicionando o backend da aplicação

---
+++ src/controllers/ItemsController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection';
+
+class ItemsController {
+    async index(request: Request, response: Response) {
+        const items = await knex('items').select('*');
+        // para funcionar no projeto mobile substitua o localhost pelo IP de onde está rodando o backend
+        const serializedItems = items.map(item => {
+            return {
+                id: item.id,
+                title: item.title,
+                image_url: `http://localhost:3333/uploads/${item.image}`
+            }
+        })
+    
+        return response.json(serializedItems);
+    }
+}
+
+export default ItemsController;
